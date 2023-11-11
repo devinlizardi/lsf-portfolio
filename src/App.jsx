4b7648ba70d3e1a48c82317376bb2d2043b3aa4d
Devin Lizardi <dlizardi@berkeley.edu>
2023-11-11 15:41:52 -0500

    scaffold project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="grid place-content-center">
+        <h1>Scaffolded Portfolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
