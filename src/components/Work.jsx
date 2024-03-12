9c6eae8e8ea544b94affc6370aeab9dc81bc76bc
Devin Lizardi <dlizardi@berkeley.edu>
2024-03-11 21:02:57 -0400

    feat: polish

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import Draggable from 'react-draggable'
 import svg from './../assets/x-icon.svg'
 
-const Work = ({ title, type, date, url }) => {
+const Work = ({ title, type, date, url, push, pop, order }) => {
   const [mode, setMode] = useState(false)
 
   return (
@@ -12,7 +12,15 @@ const Work = ({ title, type, date, url }) => {
       <button className="w-full bg-[#7f7f7f] bg-opacity-0 text-left
             hover:bg-opacity-80 hover:cursor-pointer rounded-xl flex justify-between 
             transition-all duration-200 px-4 font-light ease-in"
-        onClick={() => { setMode(!mode) }}>
+        onClick={() => {
+          if (mode) {
+            setMode(false)
+            pop()
+          } else {
+            push()
+            setMode(true)
+          }
+        }}>
         <a className="w-[155px] md:w-[270px] md:-mr-[200px]">{title}</a>
         <span className="w-[125px]">{type}</span>
         <span className="flex-none w-[75px] text-right">{date}</span>
@@ -21,15 +29,19 @@ const Work = ({ title, type, date, url }) => {
       {/* window */}
       {mode &&
         <>
-          <Draggable>
-            <div className="w-fit h-fit bg-[#757575] rounded-lg text-white absolute">
+          <Draggable onStart={push}>
+            <button
+              className="w-fit h-fit bg-[#757575] text-white absolute rounded
+               focus:border-green-400 border border-[#757575]"
+              style={{ animation: 'open 50ms', zIndex: order * 10 }}
+            >
               <div className="flex w-full justify-between items-center px-4">
                 <a className="w-[125px]">{title}</a>
                 <span className="w-[110px]">{type}</span>
                 <span className="flex-none">{date}</span>
                 <button
-                  onClick={() => { setMode(false) }}
-                  onTouchStart={() => { setMode(false) }}
+                  onMouseDown={() => { setMode(false); pop() }}
+                  onTouchStart={() => { setMode(false); pop() }}
                   className="hover:bg-[#6b6b6b] w-8 h-8 rounded-full grid 
                     place-content-center m-1 transition duration-300 ease cursor-pointer z-50">
                   <img src={svg} className="invert scale-[30%]" />
@@ -42,7 +54,7 @@ const Work = ({ title, type, date, url }) => {
                   allow="autoplay; fullscreen; picture-in-picture"
                   allowFullScreen />
               </div>
-            </div>
+            </button>
           </Draggable>
         </>
       }
--- src/components/WorkList.jsx
+++ src/components/WorkList.jsx
@@ -1,7 +1,31 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import { Work } from "./Work"
 
 const WorkList = ({ filterState, works }) => {
+  const [stack, setStack] = useState([]) // position -> zIndex
+
+  const push = (id) => {
+    const item = stack.find(i => i.id === id)
+    if (item) {
+      setStack([item, ...stack.filter(i => i.id !== id)])
+    } else {
+      setStack([{ id }, ...stack])
+    }
+  }
+
+  const pop = () => {
+    setStack(stack.slice(1))
+  }
+
+  const getOrder = (id) => {
+    const item = stack.find(i => i.id === id)
+    if (item) {
+      return stack.length - stack.indexOf(item)
+    } else {
+      return 0
+    }
+  }
 
   return (
     <>
@@ -12,7 +36,15 @@ const WorkList = ({ filterState, works }) => {
             if (filterState === 'none' || works[i].filter === filterState) {
               components.push(
                 <li key={i}>
-                  <Work title={works[i].title} type={works[i].type} date={works[i].date} url={works[i].url} />
+                  <Work
+                    title={works[i].title}
+                    type={works[i].type}
+                    date={works[i].date}
+                    url={works[i].url}
+                    push={() => push(i)}
+                    pop={pop}
+                    order={getOrder(i)}
+                  />
                 </li>
               )
             }
