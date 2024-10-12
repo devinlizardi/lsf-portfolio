29de051685c74212f086f827571bae531c6a9796
Devin Lizardi <dlizardi@berkeley.edu>
2024-10-12 16:50:16 -0700

    fix: sentry config name update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "lizzii",
-    project: "javascript-react"
+    project: "lsf-portfolio"
   })],
 
   build: {
